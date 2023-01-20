fbb6d4c5b11c86a8a94aebdeaf16eac9be240425
Sérgio Moura <smouracalmon@gmail.com>
2023-01-20 11:33:55 -0300

    Fim do colearning coletivo

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ const TESOURA = "tesoura";
 const jogadasPossiveis = [PAPEL, PEDRA, TESOURA];
 
 // CAPTURA DE ELEMENTOS ------------------------------------
-const imgPapel = document.getElementById('imgPapel') 
-const imgPedra = document.getElementById('imgPedra') 
+const imgPapel = document.getElementById('imgPapel')
+const imgPedra = document.getElementById('imgPedra')
 const imgTesoura = document.getElementById('imgTesoura')
-const imgJogadaDocuputador = document.getElementById('compChoice');
+const imgJogadaDoComputador = document.getElementById('compChoice');
 const spanPlacarUsuario = document.getElementById('userScore')
 const spanPlacarComputador = document.getElementById('compScore')
 const pPlacar = document.getElementById('scores')
 const audioDaVitoria = document.getElementById('audioDaVitoria');
 const audioDaDerrota = document.getElementById('audioDaDerrota');
-console.log(spanPlacarUsuario);
+
 /**
  * 
  * A função deve atualizar o placar na tela
@@ -26,8 +26,155 @@ console.log(spanPlacarUsuario);
  * @param {number} pontosDoUsuario 
  * @param {number} pontosDoComputador 
  */
-function atualizarPlacar(pontosDoUsuario, pontosDoComputador){
+function atualizarPlacar(pontosDoUsuario, pontosDoComputador) {
     spanPlacarUsuario.innerText = pontosDoUsuario;
     spanPlacarComputador.innerText = pontosDoComputador;
 }
+
+/**
+ * Limpa todas classe css de pPlacar e em seguida
+ * atribui a classe 'green-glow' a pPlacar
+ */
+function mostrarUsuarioVencendo() {
+    pPlacar.classList = '';
+    pPlacar.classList.add('green-glow');
+}
+
+/**
+ * Limpa todas classes css de pPlacar e em seguida
+ * atribui a classe 'red-glow a pPlacar
+ */
+function mostrarUsuarioPerdendo() {
+    pPlacar.classList = '';
+    pPlacar.classList.add('red-glow')
+}
+
+/**
+ * Limpa todas classe css de pPlacar
+ */
+function mostrarJogoEmpatado() {
+    pPlacar.classList = ""
+}
+
+/**
+ * 
+ * Retorna USUARIO caso a pontosDoUsuario seja maior
+ * que pontosDoComputador. Retorna COMPUTADOR caso
+ * pontosDoComputador seja maior que pontosDoUsuario.
+ * Retorna undefined se as pontuações forem iguais.
+ * 
+ * @param {number} pontosDoUsuario 
+ * @param {number} pontosDoComputador 
+ * @returns USUARIO | COMPUTADOR | undefined
+ */
+function quemEstaGanhando(pontosDoUsuario, pontosDoComputador) {
+    if (pontosDoUsuario > pontosDoComputador) {
+        return USUARIO;
+    } else if (pontosDoUsuario < pontosDoComputador) {
+        return COMPUTADOR;
+    } else {
+        return undefined;
+    }
+}
+
+/**
+ * Pausa o audioDaVitoria, volta para o instante zero e
+ * toca audioDaVitoria
+ */
+function alertarVitoria() {
+    audioDaVitoria.pause();
+    audioDaVitoria.currentTime = 0;
+    audioDaVitoria.play();
+}
+
+function alertarDerrota() {
+    audioDaDerrota.pause();
+    audioDaDerrota.currentTime = 0;
+    audioDaDerrota.play();
+};
+
+/**
+ * 
+ * @param {string} jogadaDoUsuario 
+ * @param {string} jogadaDoComputador 
+ * @returns USUARIO | COMPUTADOR | undefined
+ */
+function quemVenceu(jogadaDoUsuario, jogadaDoComputador) {
+
+    const combinacao = jogadaDoUsuario + jogadaDoComputador;
+
+    switch (combinacao) {
+
+        // VITÓRIA!
+        case PAPEL + PEDRA:
+        case PEDRA + TESOURA:
+        case TESOURA + PAPEL:
+            return USUARIO;
+
+        // DERROTA!
+        case PEDRA + PAPEL:
+        case TESOURA + PEDRA:
+        case PAPEL + TESOURA:
+            return COMPUTADOR
+
+        // EMPATE!
+        default:
+            return undefined;
+
+    }
+
+}
+
+/**
+ * 
+ * Aleatóriamente retorna PEDRA, PAPEL ou TESOURA
+ * 
+ * @returns PEDRA | PAPEL | TESOURA
+ */
+function jogadaAleatoria(){
+    let n = Math.floor(3 * Math.random());
+    return jogadasPossiveis[n]
+}
+
+/**
+ * Mostra a imagem respectiva no div imgJogadaDoComputador
+ * @param {PEDRA | PAPEL | TESOURA} jogadaDoComputador 
+ */
+function mostrarJogadaDoComputador(jogadaDoComputador){
+
+}
+
+function jokenpo(jogadaDoUsuario){
+    
+    let jogadaDoComputador = jogadaAleatoria();
+    let vencedor = quemVenceu(jogadaDoUsuario, jogadaDoComputador);
+    
+    if(vencedor == USUARIO) {
+        pontosDoUsuario++;
+        alertarVitoria();
+    } else if (vencedor == COMPUTADOR){
+        pontosDoComputador++;
+        alertarDerrota();
+    }
+
+    atualizarPlacar(pontosDoUsuario, pontosDoComputador);
+}
+
+
+function onPapelClick(){
+    jokenpo(PAPEL);
+}
+
+function onTesouraClick() {
+    jokenpo(TESOURA);
+}
+
+function onPedraClick(){
+    jokenpo(PEDRA);
+}
+
+imgPapel.addEventListener('click', onPapelClick);
+imgTesoura.addEventListener('click', onTesouraClick);
+imgPedra.addEventListener('click', onPedraClick);
+
 
